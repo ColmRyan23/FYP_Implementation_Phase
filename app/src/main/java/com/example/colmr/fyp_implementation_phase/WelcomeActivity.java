a33c0c7a2535d4ba0bc39b7da77a817422eb2de4
colmr <colmryan15@gmail.com>
2019-01-23 17:52:08 +0000

    Welcome Screen

--- app/src/main/java/com/example/colmr/fyp_implementation_phase/WelcomeActivity.java
+++ app/src/main/java/com/example/colmr/fyp_implementation_phase/WelcomeActivity.java
@@ -1,13 +1,24 @@
 package com.example.colmr.fyp_implementation_phase;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcomeIntent = new Intent(WelcomeActivity.this, MenuActivity.class);
+                startActivity(welcomeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
