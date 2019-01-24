e89606b4a170a620ff7d6e88c2f98211d3641708
colmr <colmryan15@gmail.com>
2019-01-24 16:43:18 +0000

    Getting started on menu

---
+++ app/src/main/java/com/example/colmr/fyp_implementation_phase/GamesMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.colmr.fyp_implementation_phase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamesMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_menu);
+    }
+}
--- app/src/main/java/com/example/colmr/fyp_implementation_phase/MenuActivity.java
+++ app/src/main/java/com/example/colmr/fyp_implementation_phase/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.colmr.fyp_implementation_phase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
+        }
+
+        public void games (View v){
+            Intent menu_item_1_intent = new Intent(MenuActivity.this, GamesMenuActivity.class);
+            startActivity(menu_item_1_intent);
+        }
 }
