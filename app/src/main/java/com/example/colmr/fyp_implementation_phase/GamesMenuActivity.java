08f48058f4a3e374397408b1241137329800d9e6
colmr <colmryan15@gmail.com>
2019-01-24 17:34:16 +0000

    Getting started on games menu

--- app/src/main/java/com/example/colmr/fyp_implementation_phase/GamesMenuActivity.java
+++ app/src/main/java/com/example/colmr/fyp_implementation_phase/GamesMenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.colmr.fyp_implementation_phase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GamesMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class GamesMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_menu);
+    }
+
+    public void gamesItem1 (View v){
+        Intent games_menu_item_1_intent = new Intent(GamesMenuActivity.this, NameFiveThingsActivity.class);
+        startActivity(games_menu_item_1_intent);
     }
 }
